2efd28880a2ef3f291aa0dac4162ceda3a622716
Sofia Rivaes <aguerreiroduarte@uoc.edu>
2026-04-15 17:58:42 +0200

    exercici 1a2 resolt

--- 1a/ej1a2.py
+++ 1a/ej1a2.py
@@ -42,9 +42,16 @@ Exemple:
 '''
 
 def sum_odd_numbers(list_numbers):
-    # Write here your code
-    pass
+    suma = 0
+    for i in list_numbers:
+        if not isinstance(i, int):
+            raise ValueError ("Numbers in the list must be integer.")
+        elif i < 0:
+            raise ValueError ("Numbers in the list must be greater or equal than 0.")
+        elif i % 2 != 0:
+            suma += i
+    return suma       
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100]))
+print(sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100]))
