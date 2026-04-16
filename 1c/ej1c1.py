7f5c2f80cbb096eca2e82b4ccf1f40ca803f530c
Sofia Rivaes <seavir@users.noreply.github.com>
2026-04-16 20:28:57 +0200

    Update ej1c1.py

--- 1c/ej1c1.py
+++ 1c/ej1c1.py
@@ -46,11 +46,18 @@ Exemple:
 
 
 def mult_recursive(value, times):
+    
+    if times == 0:
+        return 0
+    elif times == 1:
+        return value
+    else:  
+        return value + mult_recursive(value, times - 1)
     # Write here your code
-    pass
+    # pass
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-#if __name__ == "__main__":
-#    print("Must print 6: ", mult_recursive(2, 3))
+if __name__ == "__main__":
+    print("Must print 6: ", mult_recursive(2, 3))
